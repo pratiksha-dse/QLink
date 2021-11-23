0af9d89c823e0d92470c6d5f1e907156bbc8f483
palaksharma12 <77122363+palaksharma12@users.noreply.github.com>
2021-11-23 14:50:12 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+const path=require("path");
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+const publicdirectory = path.join(__dirname,'./public')
+app.use(express.static(publicdirectory));
+app.use(express.urlencoded({extented:true}));
+app.use(express.json());
+
+
+app.get('/home', function (req, res) {
+  res.render('index');
+});
+
+app.get('/', function (req, res) {
+  res.render('login');
+});
+
+app.get('/profile/post', function (req, res) {
+  res.render('post');
+});
+
+app.get('/profile/ques', function (req, res) {
+  res.render('ques');
+});
+
+app.get('/post', function (req, res) {
+  res.render('post');
+});
+app.get('/profile', function (req, res) {
+  res.render('profile');
+});
+
+app.listen(3020);
+console.log('Server is listening on port 3020');
